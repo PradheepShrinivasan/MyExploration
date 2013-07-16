ac292a9dca95ad1f709b7cfdcc7312da96e6a3a3
PradheepShrinivasan <pradheepsh@gmail.com>
2013-07-17 01:42:49 +0545

    mystring source file

---
+++ mystring/mystring.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+char* mystrcpy (const char* src, char* dest)
+{
+	char *current = dest;
+	if (!src || !dest)
+		return dest;
+
+	while (*src != '\0')
+	{
+		*current = *src;
+		++src;
+		++current;
+	}
+	*current = '\0';
+	return dest;
+}
+
+int mystrlen (const char* src)
+{
+	int length = 0;
+	while (*src) 
+	{
+		++length;
+		++src;
+	}
+	return length;
+}
+
+extern char* mystrncpy (const char* src, char* dest, int len)
+{
+	char *iter = dest;
+	if (!src || !dest || len <= 0)
+		return dest;
+
+	while (*src && len--)
+	{
+		*iter = *src;
+		++src;
+		++iter;
+	}
+	*iter = '\0';
+	return dest;
+}
